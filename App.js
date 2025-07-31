9906fd130a45fb7c798c62a0826b8517aef39513
bharaths1803 <bharaths14051803@gmail.com>
2025-07-31 17:27:22 +0530

    JSX and funcitonal comps

--- App.js
+++ App.js
@@ -4,16 +4,20 @@ import ReactDOM from "react-dom/client"
 // const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "Hello from react Js");
 // console.log("Heading", heading)
 
-const parent = React.createElement("div", {id: "parent"}, [
-  React.createElement("div", {id:"child1"}, [
-    React.createElement("h1", {}, "I'm child1 parent from h1"),
-    React.createElement("h2", {}, "I'm child1 parent from h2")
-  ]),
-  React.createElement("div", {id:"child2"}, [
-    React.createElement("h1", {}, "I'm child2 parent from h1"),
-    React.createElement("h2", {}, "I'm child2 parent from h2")
-  ]),
-])
+const heading = React.createElement("h1", {}, "I'm child1 parent from h1");
+
+
+const jsxHeading = <h1 id="heading">I'm child1 parent from h1 and from jsx</h1>;
+
+const TitleCompoent = () => <h2 id="title">I'm from titleCompoent</h2>
+
+const HeadingComponent = () => {
+  return <div className="container">
+      <h1 id="heading">I'm child1 parent from h1 and from jsx Heading</h1>
+      {heading}
+      {TitleCompoent()}
+  </div>
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
